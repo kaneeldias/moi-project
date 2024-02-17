0b96bda7c2fccbb284b74ed33c2331276088dbcf
kaneeldias <kaneeldias@hotmail.com>
2024-02-18 04:53:34 +0530

    Redesign host entity chip

--- src/components/chips/HostEntityChip.tsx
+++ src/components/chips/HostEntityChip.tsx
@@ -13,9 +13,9 @@ export default async function HostEntityChip(props: Props) {
     const host = await getHostEntity(props.opportunityId);
 
     return (
-        <div className={`flex flex-row space-x-2 text-xs`}>
+        <div className={`flex flex-row space-x-0 text-xs`}>
             <Link href={`${process.env.EXPA_URL}/committees/${host.lc.id}`} target={"_blank"}>
-                <div className={`flex flex-row space-x-1 items-center bg-blue-200 w-fit rounded-full p-1 px-2 bg-opacity-50 hover:bg-opacity-100 transition-all duration-300`}>
+                <div className={`flex flex-row space-x-1 items-center bg-blue-200 w-fit rounded-l-full p-1 pl-2 bg-opacity-50 hover:bg-opacity-100 transition-all duration-300`}>
                     <div>
                         {host.lc.name}
                     </div>
@@ -23,7 +23,7 @@ export default async function HostEntityChip(props: Props) {
             </Link>
 
             <Link href={`${process.env.EXPA_URL}/committees/${host.mc.id}`} target={"_blank"}>
-                <div className={`flex flex-row space-x-1 items-center bg-green-200 w-fit rounded-full p-1 px-2 bg-opacity-50 hover:bg-opacity-100 transition-all duration-300`}>
+                <div className={`flex flex-row space-x-1 items-center bg-green-200 w-fit rounded-r-full p-1 pr-2 bg-opacity-50 hover:bg-opacity-100 transition-all duration-300`}>
                     <div>
                         {host.mc.name}
                     </div>
