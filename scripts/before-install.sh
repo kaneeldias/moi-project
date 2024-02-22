457779f2d5b7c263afbffa723818a908f3d5eb3c
kaneeldias <kaneeldias@hotmail.com>
2024-02-23 01:39:14 +0530

    Change before install script

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -3,11 +3,11 @@
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-  nvm install 20.11.0
-  npm install --global yarn
-  npm install pm2 -g
-
-  ln -sf $(which npm) /usr/local/bin/npm
-  ln -sf $(which node) /usr/local/bin/node
-  ln -sf $(which yarn) /usr/local/bin/yarn
-  ln -sf $(which pm2) /usr/local/bin/pm2
+#  nvm install 20.11.0
+#  npm install --global yarn
+#  npm install pm2 -g
+#
+#  ln -sf $(which npm) /usr/local/bin/npm
+#  ln -sf $(which node) /usr/local/bin/node
+#  ln -sf $(which yarn) /usr/local/bin/yarn
+#  ln -sf $(which pm2) /usr/local/bin/pm2
