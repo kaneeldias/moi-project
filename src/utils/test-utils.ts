7bdfc11ecc994784e3f0a47785679081de1b5d9b
kaneeldias <kaneeldias@hotmail.com>
2024-02-26 03:11:05 +0530

    Add artificial delays

--- src/utils/test-utils.ts
+++ src/utils/test-utils.ts
@@ -1,5 +1,4 @@
 export async function waitRandomTime() {
-    return;
-    // const randomTime = Math.floor(Math.random() * 0);
-    // return new Promise(resolve => setTimeout(resolve, randomTime));
+    const randomTime = Math.floor(Math.random() * 2000);
+    return new Promise(resolve => setTimeout(resolve, randomTime));
 }
