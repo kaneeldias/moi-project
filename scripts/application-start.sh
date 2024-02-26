7f54f04a95faf817ffb20b96827c9d9caf649817
kaneeldias <kaneeldias@hotmail.com>
2024-02-26 15:43:43 +0530

    Remove artificial waits

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,10 +1,10 @@
   #!/bin/bash
   cd /var/moi-project/
-  pm2 describe npm > /dev/null
+  pm2 describe np2m > /dev/null
   RUNNING=$?
 
   if [ "${RUNNING}" -ne 0 ]; then
-    pm2 start yarn --name npm -- start
+    pm2 start yarn --name np2m -- start
   else
-    pm2 reload npm
+    pm2 reload np2m
   fi;
